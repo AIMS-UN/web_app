fa168bf9910deb54b5cfa2e0c21717c8fced73cc
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-10-16 16:51:07 -0500

    feat: :sparkles: added Material and Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppMaterialModule } from './app.material.module';
+import { ExampleComponent } from './components/example/example.component';
+import { Example2Component } from './components/example2/example2.component';
+
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, ExampleComponent, Example2Component],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AppMaterialModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
