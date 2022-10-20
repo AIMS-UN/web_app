23dd6ef705c776d019c2e360e289e5ab9aae3c47
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-10-20 00:24:12 -0500

    feat: :sparkles: Headers settled in comm. instance

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import {
+    ApolloClientOptions,
+    ApolloLink,
+    InMemoryCache,
+} from '@apollo/client/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 
 const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+    const http = httpLink.create({ uri, withCredentials: true });
+
+    const middleware = new ApolloLink((operation, forward) => {
+        operation.setContext({
+            headers: {
+                'Content-Type': 'application/json',
+                'x-forwarded-proto': 'https',
+            },
+        });
+        return forward(operation);
+    });
+
     return {
-        link: httpLink.create({ uri }),
+        link: ApolloLink.from([middleware, http]),
         cache: new InMemoryCache(),
     };
 }
