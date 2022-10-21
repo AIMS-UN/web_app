a5dc35c4cbfa310f136f01cbc29390b5fb87f205
Josué David Briceño Urquijo <29939205+jdbu2002@users.noreply.github.com>
2022-10-20 21:05:42 -0500

    feat: :sparkles: get login and register info from form
    I got the data from the inputs and put it on an object for future JSON sents :3. This little thing
    is getting bigger and bigger >:3

--- src/app/components/loginPage/loginPage.component.ts
+++ src/app/components/loginPage/loginPage.component.ts
@@ -11,4 +11,20 @@ export class LoginPageComponent implements OnInit {
     ngOnInit() {
         return;
     }
+
+    loginUser(
+        userNameField: HTMLInputElement,
+        passwordField: HTMLInputElement
+    ): boolean {
+        const username = userNameField.value;
+        const password = passwordField.value;
+
+        const data = {
+            username,
+            password,
+        };
+
+        console.log(data);
+        return false;
+    }
 }
