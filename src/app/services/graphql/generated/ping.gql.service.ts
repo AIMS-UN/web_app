0556ed91c3dcafd58bc0523e465e62510cee2b37
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-10-22 22:37:29 -0500

    feat: :sparkles: ordered generated GQL services

--- src/app/services/graphql/generated/ping.gql.service.ts
+++ src/app/services/graphql/generated/ping.gql.service.ts
@@ -1,46 +1,18 @@
+import * as Types from './types';
+
 import { Injectable } from '@angular/core';
 import * as Apollo from 'apollo-angular';
 import { gql } from 'apollo-angular';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-    [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-    [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-    [SubKey in K]: Maybe<T[SubKey]>;
-};
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
-};
-
-export type Data = {
-    __typename?: 'Data';
-    message?: Maybe<Scalars['String']>;
-    name: Scalars['String'];
-};
-
-export type QueryGetDataArgs = {
-    msName: Scalars['String'];
-};
-
-export type PingQueryVariables = Exact<{ [key: string]: never }>;
+export type PingQueryVariables = Types.Exact<{ [key: string]: never }>;
 
 export type PingQuery = { __typename?: 'Query'; ping: string };
 
-export type PongQueryVariables = Exact<{ [key: string]: never }>;
+export type PongQueryVariables = Types.Exact<{ [key: string]: never }>;
 
 export type PongQuery = { __typename?: 'Query'; pong: string };
 
-export type GetDataQueryVariables = Exact<{
-    msName: Scalars['String'];
+export type GetDataQueryVariables = Types.Exact<{
+    msName: Types.Scalars['String'];
 }>;
 
 export type GetDataQuery = {
